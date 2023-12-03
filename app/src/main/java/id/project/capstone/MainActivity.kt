67d640b8bd126a737fad882dccedcf05112c044f
wira <wirarespatibppn@gmail.com>
2023-12-03 19:00:25 +0800

    membuat form page dan open camera

--- app/src/main/java/id/project/capstone/MainActivity.kt
+++ app/src/main/java/id/project/capstone/MainActivity.kt
@@ -1,11 +1,26 @@
 package id.project.capstone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.project.capstone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.scanNow.setOnClickListener {
+            val intent = Intent(this@MainActivity, UrineFormActivity::class.java)
+            startActivity(intent)
+        }
+        binding.buttonAbout.setOnClickListener {
+            val intent = Intent(this, AboutPage::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/id/project/capstone/UrineFormActivity.kt
@@ -0,0 +1,82 @@
+package id.project.capstone
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import id.project.capstone.databinding.ActivityUrineFormBinding
+import id.project.capstone.helper.getImageUri
+
+class UrineFormActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUrineFormBinding
+
+    private var currentImageUri: Uri? = null
+
+    private val permissionRequestLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        val messageResId = if (isGranted) {
+            R.string.permission_granted
+        } else {
+            R.string.permission_denied
+        }
+        val message = getString(messageResId)
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUrineFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (!allPermissionsGranted()) {
+            permissionRequestLauncher.launch(REQUIRED_PERMISSION)
+        }
+
+        binding.toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        binding.openCamera.setOnClickListener {
+            startCamera()
+        }
+
+    }
+
+    private fun startCamera() {
+        currentImageUri = getImageUri(this)
+        launcherIntentCamera.launch(currentImageUri)
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { isSuccess ->
+        if (isSuccess) {
+            showImage()
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.imgPreview.setImageURI(it)
+        }
+    }
+
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
+    }
+}
