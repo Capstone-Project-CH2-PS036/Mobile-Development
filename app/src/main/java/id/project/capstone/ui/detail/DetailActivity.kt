424b5962c70120afda739bc4e12f371a75cdefe8
wira <wirarespatibppn@gmail.com>
2023-12-19 23:46:53 +0800

    memperbaiki detail scan history

--- app/src/main/java/id/project/capstone/ui/detail/DetailActivity.kt
+++ app/src/main/java/id/project/capstone/ui/detail/DetailActivity.kt
@@ -3,15 +3,43 @@ package id.project.capstone.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.project.capstone.R
+import id.project.capstone.databinding.ActivityDetailBinding
+import id.project.capstone.databinding.ActivityScanHistoryBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        val id = intent.getIntExtra("id",0)
-        val image = intent.getStringExtra("image")
+
+        val id = intent.getLongExtra("id",0)
         val color = intent.getStringExtra("color")
+        val desc = intent.getStringExtra("description")
+        val time = intent.getLongExtra("time",0)
+
+
+        val formattedTimestamp = formatTimestamp(time)
+
+        supportActionBar?.title = formattedTimestamp
+
+        binding.colorHistory.text = "Your Urine is "+ color
+        binding.descriptionHistory.text = desc
+        binding.toolbar.title = formattedTimestamp
+        binding.toolbar.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun formatTimestamp(timestamp: Long): String {
+        val sdf = SimpleDateFormat("dd-MMM-yyyy HH:mm:ss", Locale.getDefault())
+        val date = Date(timestamp)
+        return sdf.format(date)
     }
 }
--- app/src/main/java/id/project/capstone/ui/scanhistory/ScanHistoryActivity.kt
+++ app/src/main/java/id/project/capstone/ui/scanhistory/ScanHistoryActivity.kt
@@ -37,10 +37,12 @@ class ScanHistoryActivity : AppCompatActivity() {
         }
 
         historyAdapter.setOnItemClickListener { history ->
-            val intent = Intent(this, ResultActivity::class.java)
+            val intent = Intent(this, DetailActivity::class.java)
             intent.putExtra("id", history.id)
             intent.putExtra("image", history.image)
             intent.putExtra("color", history.color)
+            intent.putExtra("description",history.dataUrine)
+            intent.putExtra("time",history.timestamp)
             startActivity(intent)
         }
 
